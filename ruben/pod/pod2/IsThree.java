f72524b09cd984f08046105603e812da3c97c946
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-02-14 18:40:43 -0600

    added work

---
+++ ruben/pod/pod2/IsThree.java
@@ -0,0 +1,28 @@
+package pod2;
+
+//Create a function that takes three integer arguments (a, b, c) and returns the number of equal values.
+
+
+import java.sql.SQLOutput;
+
+class Challenge {
+    public static int equal(int a, int b, int c) {
+        if (a == b && b == c) {
+            System.out.println("all three match");
+            return 3;
+        }
+        if (a == b || a == c || b == c) {
+            System.out.println("Two match");
+            return 2;
+        }
+        System.out.println("none match");
+        return 0;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(equal(2,3,4));
+        System.out.println(equal(2,2,2));
+        System.out.println(equal(1,1,2));
+    }
+}
+
--- ruben/pod2/ToCamelCase.java
+++
@@ -1,4 +0,0 @@
-package pod2;
-
-import java.lang.StringBuilder;
- public class ToCamelCase {}
